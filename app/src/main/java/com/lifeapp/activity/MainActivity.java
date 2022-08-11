2504f9d8af4ea43f63b0e5181de28b89cc3f9540
helderlmc <helderlmc@gmail.com>
2022-08-11 08:58:23 -0300

    add files

---
+++ app/src/main/java/com/lifeapp/activity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.lifeapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.lifeapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    public void abrirTelaLogin(View view){
+        startActivity( new Intent(this, LoginActivity.class));
+
+
+    }
+
+    public void abrirTelaCadastro(View view){
+        startActivity( new Intent(this, CadastroActivity.class));
+
+
+    }
+
+}
